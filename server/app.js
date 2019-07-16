0ba6b4006627997b8553e49d2f98ab66ad2fdda7
Stanley <stanley.tyl@gmail.com>
2019-07-15 20:13:50 -0400

    added rootquery and graphiql setup

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express()
 
 // setup middleware
 // On the endpoint '/graphql', express will hand off the request to the graphqlHTTP function 
 app.use('/graphql', graphqlHTTP({
-
+  schema: schema, // ES6 allows you to just type 'schema'
+  graphiql: true
   })
 )
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,13 @@
 const graphql = require('graphql')
 
-const {GraphQLObjectType, GraphQLString} = graphql
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql
+
+// dummy data
+let cheeses = [
+  {name: "Mozerlla", milk: "cow", id: "1"},
+  {name: "Cheddar", milk: "cow", id: "2"},
+  {name: "Blue", milk: "goat", id: "3"},
+]
 
 const CheeseType = new GraphQLObjectType({
   name: 'Cheese',
@@ -11,3 +18,25 @@ const CheeseType = new GraphQLObjectType({
   })
 })
 
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    cheese: {
+      type: CheeseType,
+      args: {id:{type:GraphQLString}},
+      resolve(parent, args){
+        // Code to get data from DB/other source
+        for (let i of cheeses) {
+          if (i.id === args.id) {
+            return i
+          }
+        }
+
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
