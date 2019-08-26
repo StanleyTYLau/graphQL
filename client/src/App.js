a897e69bb0b498a847473b1b81c607bcc4925a74
Stanley <stanley.tyl@gmail.com>
2019-08-25 21:58:26 -0400

    adds simple cheese list component to main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+// components
+import CheeseList from "./components/CheeseList"
+
 function App() {
   return (
     <div id="main">
      <h1>Cheeses!!!</h1>
+     <CheeseList/>
     </div>
   );
 }
---
+++ client/src/components/CheeseList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function BookList() {
+  return (
+    <div>
+      <ul id="cheese-list">
+        <li>
+          Cheese name
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;
