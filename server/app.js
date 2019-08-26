bc36c52cba53431d3c62263c5c2372ac1b4e80af
Stanley <stanley.tyl@gmail.com>
2019-08-25 21:29:37 -0400

    changes port for server

--- server/app.js
+++ server/app.js
@@ -20,6 +20,6 @@ app.use('/graphql', graphqlHTTP({
 )
 
 app.listen(4000, () => {
-  console.log("Now listening for requests on port 3000")
+  console.log("Now listening for requests on port 4000")
 })
 
